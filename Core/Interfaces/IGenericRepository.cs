ea0afdcbe9b7022d241be4fb0b43d750c9bf728c
Khalil El Mhamdi <khalilelmhamdi3@gmail.com>
2022-03-17 12:24:31 +0100

    17/03/22

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Insert(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
---
+++ Infrastructure/Repository/GenericRepsitory.cs
@@ -0,0 +1,48 @@
+﻿using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class GenericRepsitory<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DataContext _dataContext;
+        private DbSet<T> table = null;
+        public GenericRepsitory(DataContext context)
+        {
+           _dataContext = context;
+            table = _dataContext.Set<T>();
+
+
+        }
+        IEnumerable<T> IGenericRepository<T>.GetAll()
+        {
+           return  table.ToList();
+        }
+        void IGenericRepository<T>.Delete(int id)
+        {
+          
+        }
+
+        T IGenericRepository<T>.Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+        void IGenericRepository<T>.Insert(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IGenericRepository<T>.Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
